d1a115df8965d4ef375ab49faaf4cfe0cc3ce0b0
Artic137 <51089633+Artic137@users.noreply.github.com>
2019-05-27 17:19:25 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var tabla_artic
+var tabla_solarblair
+
+client.on('ready', () => {
+   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {	
+  if (msg.content.startsWith("/consulta")) {
+	funcion_consultar(msg)
+  }
+});
+
+function funcion_consultar(msg) {
+  msg.reply('Este es el n\u00FAmero de veces que has utilizado cada personaje: \n' 
+  );
+};
+
+client.login(process.env.NTgyMjM1NjQxNDU1MTgxODUw.XOvTfA.in0Q4gFfackJXRwsFc8oftLQ_a8);
+
+
+
